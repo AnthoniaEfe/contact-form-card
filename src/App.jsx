37171fc8242a595df6dcc338cc1941da66d95122
AnthoniaEfe <anthoniaefe36@gmail.com>
2025-03-21 15:14:41 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./styles/styles.scss"
+
+function App() {
+
+
+  return (
+    <>
+   <main>
+    <div id="card-container">
+      <header>
+         <h1>Contact Us</h1>
+      </header>
+
+      <form>
+
+      </form>
+    </div>
+   </main>
+    </>
+  )
+}
+
+export default App
